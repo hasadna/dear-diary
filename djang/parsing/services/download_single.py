20a10898077b7715e28c602a7dd64856ff6047ef
Nitzan Raz <nitz.raz@gmail.com>
2023-02-28 11:54:15 +0200

    moved try/except block a bit deeper

--- djang/parsing/services/download_single.py
+++ djang/parsing/services/download_single.py
@@ -45,23 +45,16 @@ def get_package_name(resource, website):
     return j['result']['title']
 
 def process_resource_impl(resource, website, force):
-    content = get_resource_content(resource)
-    calendar_name = get_package_name(resource, website=website)
-    process_calendar(
-        resource_id=resource.id,
-        calendar_name=calendar_name,
-        when_created_at_source=resource.when_created,
-        file_stream=content,
-        force=force,
-    )
-
-def process_resource(resource_id, website, force):
     try:
-        resource = get_resource(
-            resource_id=resource_id,
-            website=website,
+        content = get_resource_content(resource)
+        calendar_name = get_package_name(resource, website=website)
+        process_calendar(
+            resource_id=resource.id,
+            calendar_name=calendar_name,
+            when_created_at_source=resource.when_created,
+            file_stream=content,
+            force=force,
         )
-        process_resource_impl(resource, website, force)
     except Exception as e:
         status = "Exception"
         detail = traceback.format_exc()
@@ -74,5 +67,12 @@ def process_resource(resource_id, website, force):
         status=status,
         detail=detail
     ).save()
+
+def process_resource(resource_id, website, force):
+    resource = get_resource(
+        resource_id=resource_id,
+        website=website,
+    )
+    process_resource_impl(resource, website, force)
 
 
